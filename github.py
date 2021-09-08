c78360cebaf9b6c37555f546a3048ee740df7f58
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-08 16:12:40 +0200

    created

---
+++ github.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,request
+import requests
+
+
+app = Flask(__name__)
+base_url = "https://api.github.com/users/"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        githubname = request.form.get("githubname")
+        response = requests.get(base_url+githubname)
+        user_info = response.json()
+        if "message" in user_info:
+            return render_template("index.html", error="User not found...")
+        return render_template("index.html", profile=user_info)
+
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
